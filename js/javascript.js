c3f11d592ae9c26a573df8dbfb10e4dda0237a10
ErykHonda <eryk.minecraft@outlook.com>
2025-01-21 16:52:12 +0100

    add img

--- js/javascript.js
+++ js/javascript.js
@@ -1,47 +1,58 @@
 // Array of photo objects
-const photos = [
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 1' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 2' },
-    { src: 'img/lEDPIy5FVN-jIHPoEzs_V.jpg', alt: 'Zdjęcie 3' }
-  ];
-  
-  // Function to create photo gallery
-  function createPhotoGallery() {
-    const galleryContainer = document.getElementById('photo-gallery');
-    
-    // Loop through the photos array and create HTML elements
-    photos.forEach(photo => {
-      const colDiv = document.createElement('div');
-      colDiv.classList.add('col-12', 'col-sm-6', 'col-lg-4', 'photo-container');
-      
-      const img = document.createElement('img');
-      img.src = photo.src;
-      img.alt = photo.alt;
-      img.classList.add('photo', 'img-fluid');
-      img.setAttribute('data-bs-toggle', 'modal');
-      img.setAttribute('data-bs-target', '#photoModal');
-      img.onclick = () => showImage(photo.src);
-      
-      colDiv.appendChild(img);
-      galleryContainer.appendChild(colDiv);
-    });
-  }
-  
-  // Function to display the image in the modal
-  function showImage(imageSrc) {
-    const modalImage = document.getElementById('modalImage');
-    modalImage.src = imageSrc;
-  }
-  
-  // Create the gallery when the page is ready
-  document.addEventListener('DOMContentLoaded', createPhotoGallery);
-  
+const photos1 = [
+  { src: "img/graf/graf_1.jpg", alt: "Zdjęcie 1" },
+  { src: "img/graf/graf_2.jpg", alt: "Zdjęcie 2" },
+  { src: "img/graf/graf_3.jpg", alt: "Zdjęcie 3" },
+  { src: "img/graf/graf_4.jpg", alt: "Zdjęcie 4" },
+];
+
+const photos2 = [
+  { src: "img/inst/inst_1.jpg", alt: "Zdjęcie 1" },
+  { src: "img/inst/inst_2.jpg", alt: "Zdjęcie 2" },
+  { src: "img/inst/inst_3.jpg", alt: "Zdjęcie 3" },
+  { src: "img/inst/inst_4.jpg", alt: "Zdjęcie 4" },
+  { src: "img/inst/inst_5.jpg", alt: "Zdjęcie 5" },
+  { src: "img/inst/inst_6.jpg", alt: "Zdjęcie 6" },
+  { src: "img/inst/inst_7.jpg", alt: "Zdjęcie 7" },
+  { src: "img/inst/inst_8.jpg", alt: "Zdjęcie 8" },
+  { src: "img/inst/inst_9.jpg", alt: "Zdjęcie 9" },
+  { src: "img/inst/inst_10.jpg", alt: "Zdjęcie 10" },
+];
+
+// Function to create photo gallery
+function createPhotoGallery(photos, galleryContainerId) {
+  const galleryContainer = document.getElementById(galleryContainerId);
+
+  // Loop through the photos array and create HTML elements
+  photos.forEach((photo) => {
+    const colDiv = document.createElement("div");
+    colDiv.classList.add("col-12", "col-sm-6", "col-lg-4", "photo-container");
+
+    const img = document.createElement("img");
+    img.src = photo.src;
+    img.alt = photo.alt;
+    img.title = photo.alt;
+    img.classList.add("photo", "img-fluid");
+    img.setAttribute("data-bs-toggle", "modal");
+    img.setAttribute("data-bs-target", "#photoModal");
+    img.onclick = () => showImage(photo.src, photo.alt);
+
+    colDiv.appendChild(img);
+    galleryContainer.appendChild(colDiv);
+  });
+}
+
+function showImage(imageSrc, imageAlt) {
+  const modalImage = document.getElementById("modalImage");
+  const modalTitle = document.getElementById("modalTitle");
+  modalImage.src = imageSrc;
+  modalTitle.innerHTML = imageAlt;
+}
+
+// Create the gallery when the page is ready
+document.addEventListener("DOMContentLoaded", () =>
+  createPhotoGallery(photos1, "photo-gallery_graf")
+);
+document.addEventListener("DOMContentLoaded", () =>
+  createPhotoGallery(photos2, "photo-gallery_inst")
+);
